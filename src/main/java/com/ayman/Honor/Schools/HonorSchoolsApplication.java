ef6d8149f45b6c1d154c2c6abd9f32383de236de
Ayman Hazem <aymooongsr2@gmail.com>
2025-04-12 06:49:24 +0200

    init commit

---
+++ src/main/java/com/ayman/Honor/Schools/HonorSchoolsApplication.java
@@ -0,0 +1,13 @@
+package com.ayman.Honor.Schools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonorSchoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonorSchoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayman/Honor/Schools/HonorSchoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayman.Honor.Schools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonorSchoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
