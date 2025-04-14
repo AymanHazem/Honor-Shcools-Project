c9d156850c53d7b9183508dc6298c0c3fbaf9d8f
Ayman Hazem <aymooongsr2@gmail.com>
2025-04-14 16:43:22 +0200

    Add User And Admin with inMemoryAuthentication()

--- src/main/java/com/ayman/Honor/Schools/config/ProjectSecurityConfig.java
+++ src/main/java/com/ayman/Honor/Schools/config/ProjectSecurityConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -28,5 +32,12 @@ public class ProjectSecurityConfig
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService()
+    {
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("123").roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("123").roles("USER","ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
